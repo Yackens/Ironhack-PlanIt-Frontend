a86abbc82c4356e73ee017cc0e746a42c83e50d8
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-08-07 11:33:37 +0200

    signup react done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,42 +1,67 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function SignUp() {
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const nav = useNavigate();
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5005/auth/signup", {
+        email,
+        password,
+        username,
+      });
+      console.log("here is the signup response", res);
+      nav("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
-        <div id="logo">
-            <Link to='/'>
-                <p>PlanIt</p>
-            </Link>
-         </div>
-
-        <h2>SignUp</h2>
-
-        <form>
-    <label>
-      E-Mail
-      <input/>
-    </label>
-    </form>
-
-        <form>
-    <label>
-      Username
-      <input/>
-    </label>
-    </form>
-
-    <form>
-    <label>
-      Password
-      <input/>
-    </label>
-    </form>
-
-    <button>SignUp</button>
-        
+      <h2>Sign Up Page</h2>
+      <form onSubmit={handleSignup}>
+        <label>
+          username:
+          <input
+            type="text"
+            value={username}
+            required
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            required
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            required
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">Signup</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default Signup;
