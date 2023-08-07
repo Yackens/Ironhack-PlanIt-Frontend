051ba4d949b564d3e458ad746fd0a60370615c27
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-08-07 16:38:04 +0200

    added textarea to NewTask

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -5,11 +5,10 @@ import NavBar from '../Components/NavBar';
 function NewTask() {
   const [taskName, setTaskName] = useState('');
   const [description, setDescription] = useState('');
-  const categoryName = "CATEGORY NAME"; // Hier den tatsächlichen Kategorienamen einsetzen
-
+  
   const handleCreateTask = () => {
     // Handle creating the task and submitting to MongoDB
-    // You can use the state variables taskName and description
+  
     console.log('Create Task clicked');
     console.log('Task Name:', taskName);
     console.log('Description:', description);
@@ -24,7 +23,8 @@ function NewTask() {
       </div>
       
       <NavBar />
-      <h2>{categoryName}</h2>
+      {/**needs to be done */}
+      <h2>categoryName</h2>
       <h3>Create New Task</h3>
 
       <form>
@@ -40,7 +40,7 @@ function NewTask() {
       <form>
         <label>
           Description
-          <input
+          <textarea
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
