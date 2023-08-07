11bcc39df7e8eaa2852a1d1b7c4410bc3f856872
Alexander Alexy <ax81@airvonalexander.localdomain>
2023-08-07 16:17:35 +0200

    NewTask done = CRUD done > without testing

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -1,36 +1,55 @@
-import React from 'react'
-import NavBar from '../Components/NavBar'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../Components/NavBar';
 
 function NewTask() {
+  const [taskName, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
+  const categoryName = "CATEGORY NAME"; // Hier den tatsächlichen Kategorienamen einsetzen
+
+  const handleCreateTask = () => {
+    // Handle creating the task and submitting to MongoDB
+    // You can use the state variables taskName and description
+    console.log('Create Task clicked');
+    console.log('Task Name:', taskName);
+    console.log('Description:', description);
+  };
+
   return (
     <div>
+      <div id="logo">
+        <Link to='/'>
+          <p>PlanIt</p>
+        </Link>
+      </div>
+      
+      <NavBar />
+      <h2>{categoryName}</h2>
+      <h3>Create New Task</h3>
 
-        <div id="logo">
-            <Link to='/'>
-                <p>PlanIt</p>
-            </Link>
-         </div>    
+      <form>
+        <label>
+          Task name
+          <input
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+          />
+        </label>
+      </form>
 
-        <NavBar />
-         <h2>CATEGORY NAME</h2>
-         <h3>Create New Task</h3>
+      <form>
+        <label>
+          Description
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+      </form>
 
-         <form>
-    <label>
-      Task name
-      <input/>
-    </label>
-    </form>
-
-    <form>
-    <label>
-      Description
-      <input/>
-    </label>
-    </form>
+      <button onClick={handleCreateTask}>Create Task</button>
     </div>
-  )
+  );
 }
 
-export default NewTask
+export default NewTask;
