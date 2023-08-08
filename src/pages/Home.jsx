d6af891efc1e41aec53e893ad3cbadd1ca05e095
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-08-08 16:55:49 +0200

    Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,43 @@
-//Home.jsx
-
-import React from 'react'
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from './context/Auth.context';
-import { API_URL } from '../config/vite.config';
-
-
 function Home() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   
-   //this is how we grab things from the context
-   const authContext = useContext(AuthContext)
-   const { authenticateUser } = authContext;
-   const nav = useNavigate();
-   const handleLogin = async (e) => {
-     e.preventDefault();
-     try {
-       const { data } = await axios.post(`${API_URL}/auth/login`, {
-         username,
-         password,
-       });
-       console.log("here is the Login response", data);
-       localStorage.setItem("authToken", data.token);
-       
-       await authenticateUser();
-       nav("/categories");
-     } catch (err) {
-       console.log(err);
-       setErrorMessage(err.response.data.errorMessage);
-     }
-   };
+  const authContext = useContext(AuthContext)
+  const { authenticateUser } = authContext;
+  const nav = useNavigate();
+  
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(`${API_URL}/auth/login`, {
+        username,
+        password,
+      });
+      console.log("here is the Login response", data);
+      localStorage.setItem("authToken", data.token);
+      await authenticateUser();
+      nav("/categories");
+    } catch (err) {
+      console.log(err);
+      setErrorMessage(err.response.data.errorMessage);
+    }
+  };
 
   return (
-   <div>
-    
-        <div id="logo">
-            <Link to='/'>
-                <p>PlanIt</p>
-            </Link>
-         </div>
-    
-// needs to be added
-    <h1>Hey USER Just plan it!</h1>
-
-    <form onSubmit={handleLogin}>
-    <label>
-      Username
-      <input
+    <div>
+      <div id="logo">
+        <Link to='/'>
+          <p>PlanIt</p>
+        </Link>
+      </div>
+      
+      <h1>Hey USER Just plan it!</h1>
+      
+      <form onSubmit={handleLogin}>
+        <label>
+          Username
+          <input
             type="text"
             value={username}
             required
@@ -59,11 +45,9 @@ function Home() {
               setUsername(event.target.value);
             }}
           />
-    </label>
-    </form>
+        </label>
 
-    <form>
-    <label>
+        <label>
           Password:
           <input
             type="password"
@@ -74,15 +58,15 @@ function Home() {
             }}
           />
         </label>
-    </form>
-
-    <button type="submit">LogIn</button>
-    <Link to="/signup">
+        
+        <button type="submit">LogIn</button>
+      </form>
+      
+      <Link to="/signup">
         <button>SignUp</button>
       </Link>
     </div> 
-  )
-
+  );
 }
 
-export default Home
+export default Home;
