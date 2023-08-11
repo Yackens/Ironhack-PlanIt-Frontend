48b7147f23c6c3ba9617a547f16d53264da88447
Joaquin Maroto Pascual <joaquinmarpas@gmail.com>
2023-08-11 15:48:47 +0200

    Changes NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../pages/context/Auth.context";
 
-
 function Navbar() {
   const authContext = useContext(AuthContext);
 
